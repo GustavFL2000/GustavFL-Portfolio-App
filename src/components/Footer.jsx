f7062a886623b10fad533eab642eccb0de17b7ed
Gustav Færmann Lassen <gustavfl13@gmail.com>
2025-10-28 10:14:49 +0100

    Linkdin link i footeren

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,10 @@ function Footer() {
     <footer className="footer">
       <p>© {new Date().getFullYear()} Gustav Færmann Lassen</p>
       <p>
+        Linkdin:{" "} 
+        <a href = "https://www.linkedin.com/in/gustav-faermann-lassen">gustav-faermann-lassen</a>
+      </p>
+      <p>
         Lavet i React | Kontakt:{" "}
         <a href="mailto:gustavfl13@gmail.com">gustavfl13@gmail.com</a>
       </p>
