24e76da92f75a536757cac3f5a1ae199e94c1958
Gustav Færmann Lassen <gustavfl13@gmail.com>
2025-11-03 15:31:31 +0100

    style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Header.css";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header className="header">
       <nav>
-        <Link to="/">Hjem</Link>
-        <Link to="/Games">Spil</Link>
-        <Link to="/Projects">Projekter</Link>
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+          Hjem
+        </Link>
+        <Link
+          to="/Games"
+          className={location.pathname === "/Games" ? "active" : ""}
+        >
+          Spil
+        </Link>
+        <Link
+          to="/Projects"
+          className={location.pathname === "/Projects" ? "active" : ""}
+        >
+          Projekter
+        </Link>
       </nav>
     </header>
   );
